ab69a8d8a05da7dbb59657bf25621040e458b7f6
nbremer <nadiehbremer@gmail.com>
2018-06-25 19:31:32 -0400

    Put in the empty start script

--- js/script.js
+++ js/script.js
@@ -24,81 +24,48 @@ const colorsBlue = ['#0d4982','#1d6092','#2c79a1','#3294a5','#1ab29d']
 //////////////////////// Create SVG //////////////////////////
 //////////////////////////////////////////////////////////////
 
-const margin = {
-    left: 20,
-    top: 20,
-    right: 20,
-    bottom: 20,
-}
-const width = 1250 - margin.left - margin.right
-const height = 900 - margin.top - margin.bottom
+// const margin = {
+//     left: 20,
+//     top: 20,
+//     right: 20,
+//     bottom: 20,
+// }
+// const width = 1250
+// const height = 900
 
-const svg = d3.select("#chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + [margin.left + width/2, margin.bottom + height/2] + ")")
+// const svg
 
 //////////////////////////////////////////////////////////////
 ////////////////////// Create scales /////////////////////////
 //////////////////////////////////////////////////////////////
 
-const timeScale = d3.scaleLinear()
-    .domain([0, numData-1])
-    .range([0.025 * pi2, (1 - 0.025) * pi2])
+// const timeScale
 
-const birthScale = d3.scaleLinear()
-    .domain([0, maxBabies])
-    .range([0, height/2])
+// const birthScale
 
-const line = d3.radialLine()
-    .angle(d => timeScale(d.time))
-    .radius(d => birthScale(d.line))
+// const line
 
-const areaScale = d3.radialArea()
-    .angle(d => timeScale(d.time))
-    .innerRadius(d => birthScale(d.line))
-    .outerRadius(() => birthScale(averageBabies))
-
+// const areaScale
 
 ////////////////////////////////////////////////////////////// 
 ///////////////////// Create SVG effects ///////////////////// 
 //////////////////////////////////////////////////////////////
 
-const defs = svg.append("defs")
+// const defs = svg.append("defs")
 
-const filter = defs.append("filter")
-    .attr("id", "shadow")
-
-filter.append("feColorMatrix")
-    .attr("type", "matrix")
-    .attr("values", "0 0 0 0   0 0 0 0 0   0 0 0 0 0   0 0 0 0 0.1 0")
-filter.append("feGaussianBlur")
-    .attr("stdDeviation","5")
-    .attr("result","coloredBlur")
-const feMerge = filter.append("feMerge")
-feMerge.append("feMergeNode").attr("in","coloredBlur")
-feMerge.append("feMergeNode").attr("in","SourceGraphic")
+// const filter
+//.attr("id", "shadow")
 
 //Create background "chart-area" circle
-svg.append("circle")
-    .attr("class", "chart-area")
-    .attr("r", birthScale(outerCircleShadow))
-    .style("filter", "url(#shadow)")
+//.attr("class", "chart-area")
 
 //////////////////////////////////////////////////////////////
 ///////////////////////// Add title //////////////////////////
 //////////////////////////////////////////////////////////////
 
-svg.append("text")
-    .attr("class", "title-top")
-    .attr("y", -5)
-    .text("Minutes")
-    
-svg.append("text")
-    .attr("class", "title-bottom")
-    .attr("y", 15)
-    .text("per day")
+//.attr("class", "title-top")
+
+//.attr("class", "title-bottom")
 
 //////////////////////////////////////////////////////////////
 ////////////////////// Draw time labels //////////////////////
@@ -123,18 +90,18 @@ svg.append("text")
 //////////////////////////////////////////////////////////////
 
 //data: data/SciAm_minute_per_day_2014.csv
-d3.csv("data/SciAm_minute_per_day_2014.csv").then(babyData => {
 
     //////////////////////////////////////////////////////////////
     ///////////////////// Final data prep ////////////////////////
     //////////////////////////////////////////////////////////////
 
-    babyData.forEach(d => {
-        d.time = +d.time
-        d.births = +d.births
-        d.line = +d.line
-    })
-    console.table(babyData[0])
+    // console.table(babyData[0])
+
+    // babyData.forEach(d => {
+    //     d.time = +d.time
+    //     d.births = +d.births
+    //     d.line = +d.line
+    // })
 
     //////////////////////////////////////////////////////////////
     /////////////////////// Draw circles /////////////////////////
@@ -145,60 +112,36 @@ d3.csv("data/SciAm_minute_per_day_2014.csv").then(babyData => {
     // y = radius * sin(angle)
 
     //class: circle-group
-    const circles = svg.append("g").attr("class", "circle-group")
+    // const circles
 
-    circles.selectAll(".circle")
-        .data(babyData)
-        .enter().append("circle")
-        .attr("class","circle")
-        .attr("cx", d => birthScale(d.births) * Math.cos(timeScale(d.time) - pi1_2))
-        .attr("cy", d => birthScale(d.births) * Math.sin(timeScale(d.time) - pi1_2))
-        .attr("r", dotRadius)
+    //.attr("class","circle")
 
     //////////////////////////////////////////////////////////////
     ////////////////////// Create clip paths /////////////////////
     //////////////////////////////////////////////////////////////
 
-    const clips = svg.append("g").attr("class", "clip-group")
+    // const clips = svg.append("g").attr("class", "clip-group")
 
-    clips.append("clipPath")
-            .attr("id", "clip-area")
-            .append("path")
-            .attr("d", areaScale(babyData))
+    //.attr("id", "clip-area")
 
     //////////////////////////////////////////////////////////////
     ////////////////////// Draw LOESS line ///////////////////////
     //////////////////////////////////////////////////////////////
 
-    // const loessLine = svg.append("path")
-    //     .attr("d", line(babyData))
-    //     .style("fill", "none")
-    //     .style("stroke", "hotpink")
-    //     .style("stroke-width", 3)
+    // const loessLine
 
     //////////////////////////////////////////////////////////////
     //////////////////////// Draw areas //////////////////////////
     //////////////////////////////////////////////////////////////
 
-    const area = svg.append("g").attr("class", "area-group")
-        .attr("clip-path", "url(#clip-area)")
+    // const area = svg.append("g").attr("class", "area-group")
+    // .attr("class", "area")
 
-    area.selectAll(".color-circle-above")
-        .data(colorsRed.reverse())
-        .enter().append("circle")
-        .attr("class", "color-circle-above")
-        .attr("r", (d,i) => birthScale(maxBabies) - (birthScale(maxBabies) - birthScale(averageBabies))/colorsRed.length * i)
-        .style("fill", d => d)
-        .style("filter", "url(#shadow)")
-        
+    //.attr("class", "color-circle-above")
+    //.attr("r", (d,i) => birthScale(maxBabies) - (birthScale(maxBabies) - birthScale(averageBabies))/colorsRed.length * i)
 
-    area.selectAll(".color-circle-below")
-        .data(colorsBlue.reverse())
-        .enter().append("circle")
-        .attr("class", "color-circle-below")
-        .attr("r", (d,i) => birthScale(averageBabies) - (birthScale(averageBabies) - birthScale(minBabies))/colorsBlue.length * i)
-        .style("fill", d => d)
-        .style("filter", "url(#shadow)")
+    //.attr("class", "color-circle-below")
+    //.attr("r", (d,i) => birthScale(averageBabies) - (birthScale(averageBabies) - birthScale(minBabies))/colorsBlue.length * i)
 
     //////////////////////////////////////////////////////////////
     ///////////////////// Draw gridlines /////////////////////////
@@ -217,24 +160,12 @@ d3.csv("data/SciAm_minute_per_day_2014.csv").then(babyData => {
     // const circlesTop = svg.append("g").attr("class", "circle-group")
     //class: circle-top
 
-    const circlesTop = svg.append("g").attr("class", "circle-group")
-        .attr("clip-path", "url(#clip-area)")
-
-    circlesTop.selectAll(".circle-top")
-        .data(babyData)
-        .enter().append("circle")
-        .attr("class","circle-top")
-        .attr("cx", d => birthScale(d.births) * Math.cos(timeScale(d.time) - pi1_2))
-        .attr("cy", d => birthScale(d.births) * Math.sin(timeScale(d.time) - pi1_2))
-        .attr("r", dotRadius)
-
     //////////////////////////////////////////////////////////////
     ///////////////////////// Draw lines /////////////////////////
     //////////////////////////////////////////////////////////////
 
-    const averageLine = svg.append("path")
-        .attr("class", "average-line")
-        .attr("d", arcPath(birthScale(averageBabies)))
+    // const averageLine
+    //.attr("class", "average-line")
 
     ////////////////////////////////////////////////////////////// 
     ///////////////////////// Annotations ////////////////////////
@@ -246,22 +177,19 @@ d3.csv("data/SciAm_minute_per_day_2014.csv").then(babyData => {
     
     // const makeAnnotations
 
-})
-
 ////////////////////////////////////////////////////////////// 
 /////////////////////// Helper functions /////////////////////
 ////////////////////////////////////////////////////////////// 
 
-function arcPath(r) {
-    // //When you know the radius and angle, you get back to x & y pixels by:
-    // x = radius * cos(angle)
-    // y = radius * sin(angle)
-    let xStart = r * Math.cos(timeScale(0) - pi1_2)
-    let yStart = r * Math.sin(timeScale(0) - pi1_2)
-    let xEnd = r * Math.cos(timeScale(numData-1) - pi1_2)
-    let yEnd = r * Math.sin(timeScale(numData-1) - pi1_2)
+// function arcPath(r) {
+//     // //When you know the radius and angle, you get back to x & y pixels by:
+//     // x = radius * cos(angle)
+//     // y = radius * sin(angle)
+//     let xStart = r * Math.cos(timeScale(0) - pi1_2)
+//     let yStart = r * Math.sin(timeScale(0) - pi1_2)
+//     let xEnd = r * Math.cos(timeScale(numData-1) - pi1_2)
+//     let yEnd = r * Math.sin(timeScale(numData-1) - pi1_2)
 
-    //M start-x, start-y A radius-x, radius-y, x-axis-rotation, large-arc-flag, sweep-flag, end-x, end-y //(0 1 1)
-    return "M" + [xStart, yStart] + " A " + [r,r] + " 0 1 1 " + [xEnd, yEnd]
+//     //(0 1 1)
 
-}//function arcPath
+// }//function arcPath
